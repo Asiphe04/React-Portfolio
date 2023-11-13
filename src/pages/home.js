eb9b3a727e3db35c85530f59659028e1383e47cd
Asiphe04 <asiphendimlana40@gmail.com>
2023-11-13 16:26:55 +0200

    halfway with home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,14 +5,20 @@ function Home(){
         <>   
      <div className='container'>
       <div className='first-div'>
+    
+      
       <h1>ASIPHE NDIMLANA</h1>
+      <img src='https://i.postimg.cc/Hnr5rkGy/Avatar-Wizard.webp' alt='Troop' className='troop'/>
       <p className='desc' id='role-desc'>Web Developer, Cloud practitioner, Problem Solver</p>
       <p className='desc'>Hi, I am Lorem Ipsum, I design lorem ipsums for people to use everyday. start your day off well with a fresh lorem ipsum dolor sit amet </p>
       <button className='btn-projects'>My Work</button>
       <button className='btn-contact'>Contact Me</button>
+      <img src='https://i.postimg.cc/Hnr5rkGy/Avatar-Wizard.webp' alt='Troop' className='troop'/>
       </div>
       <div className='img-div'>
+      <img src='https://i.postimg.cc/NfRCdrkN/Avatar-Wall-Wrecker.webp' alt='Troop' className='troop'/>
       <img src='https://i.postimg.cc/h465q5qj/image-removebg-preview.png' alt='Asiphe Ndimlana' className='Home-img'/>
+      <img src='https://i.postimg.cc/cLZ924Dd/Avatar-Minion.webp' alt='Troop' className='troop'/>
       </div>
      
      </div>
