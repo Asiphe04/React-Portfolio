d26c0c8baf1e00b9b101391b4734f7b1e996de18
Asiphe04 <asiphendimlana40@gmail.com>
2023-11-13 09:56:49 +0200

    busy with home page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
       <Footer/>
     </div>
   );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,24 @@
-import './Footer.css'
+import './home.css'
 
-function Footer(){
+function Home(){
     return (
         <>   
-      <footer>
-      <p> Copyright &copy; 2023 All Rights Reserved</p>
-      </footer>
+     <div className='container'>
+      <div className='first-div'>
+      <h1>ASIPHE NDIMLANA</h1>
+      <p className='desc' id='role-desc'>Web Developer, Cloud practitioner, Problem Solver</p>
+      <p className='desc'>Hi, I am Lorem Ipsum, I design lorem ipsums for people to use everyday. start your day off well with a fresh lorem ipsum dolor sit amet </p>
+      <button className='btn-projects'>My Work</button>
+      <button className='btn-contact'>Contact Me</button>
+      </div>
+      <div className='img-div'>
+      <img src='https://i.postimg.cc/h465q5qj/image-removebg-preview.png' alt='Asiphe Ndimlana' className='Home-img'/>
+      </div>
+     
+     </div>
+     
         </>
     )
 }
 
-export default Footer;
+export default Home;
