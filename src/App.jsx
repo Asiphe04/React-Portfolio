f50a38922b80f7dd0086fe8d476000e5f1476914
Asiphe04 <asiphendimlana40@gmail.com>
2023-11-17 14:19:09 +0200

    working on home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar'
 import './App.css'
+import Footer from './components/Footer'
+import Home from './pages/Home'
 
 function App() {
 
 
   return (
-   <div>
-    
+   <div className='container'>
+    <Navbar/>
+    <Home/>
+    <Footer/>
    </div>
   )
 }
