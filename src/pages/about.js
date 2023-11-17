cd12328ca213d63860b281c25bcdba5090fb9e26
Asiphe04 <asiphendimlana40@gmail.com>
2023-11-17 09:01:32 +0200

    added about me details

--- src/pages/about.js
+++ src/pages/about.js
@@ -27,8 +27,55 @@ function About(){
   </label>
       
 </div>
+<div className='about-me-details'>
+<div className='arrow-info'>
 <img src='https://i.postimg.cc/85zGRd2r/Arrow-6.png' className='arrow-img' alt='Arrow'/>
+<p>Full Name:</p>
+<p>Asiphe Ndimlana</p>
+</div>
+
+<div className='arrow-info'>
+<img src='https://i.postimg.cc/85zGRd2r/Arrow-6.png' className='arrow-img' alt='Arrow'/>
+<p>DOB:</p>
+<p>03/09/2004</p>
+</div>
+<div className='arrow-info'>
+<img src='https://i.postimg.cc/85zGRd2r/Arrow-6.png' className='arrow-img' alt='Arrow'/>
+<p>Languages:</p>
+<p>Xhosa ANd English</p>
+</div>
+<div className='arrow-info'>
+<img src='https://i.postimg.cc/85zGRd2r/Arrow-6.png' className='arrow-img' alt='Arrow'/>
+<p>Gender:</p>
+<p>Male</p>
+</div>
+</div>
+<div className='contact-info'>
+<div className='arrow-info'>
+<img src='https://i.postimg.cc/85zGRd2r/Arrow-6.png' className='arrow-img' alt='Arrow'/>
+<p>Cell No:</p>
+<p>0781149972</p>
+</div>
+<div className='arrow-info'>
+<img src='https://i.postimg.cc/85zGRd2r/Arrow-6.png' className='arrow-img' alt='Arrow'/>
+<p>Email:</p>
+<p>ASIPHENDIMLANA40@GMAIL.COM</p>
+</div >
+<div className='arrow-info'>
+<img src='https://i.postimg.cc/85zGRd2r/Arrow-6.png' className='arrow-img' alt='Arrow'/>
+<p>LinkedIn:</p>
+<p>https://www.linkedin.com/in/asiphe-ndimlana-045184157/</p>
+</div>
+<div className='arrow-info'>
+<img src='https://i.postimg.cc/85zGRd2r/Arrow-6.png' className='arrow-img' alt='Arrow'/>
+<p>Availability:</p>
+<p>Any time</p>
+</div>
+</div>
+
+
     </div>
+    
 </div>
    
 
