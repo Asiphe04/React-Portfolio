df3ee80adaca32312ed402202ca98c746dc39032
Asiphe04 <asiphendimlana40@gmail.com>
2023-11-20 15:57:21 +0200

    done with about layout

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -37,23 +37,23 @@ const About = () => {
               <div>
                 <div className='arrow-info'>
                   <img src='https://i.postimg.cc/85zGRd2r/Arrow-6.png' className='arrow-img' alt='Arrow'/>
-                  <p>Full Name: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</p>
-                  <p>Asiphe Ndimlana</p>
+                  <p className='details'>Full Name: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</p>
+                  <p className='details'>Asiphe Ndimlana</p>
                 </div>
                 <div className='arrow-info'>
                   <img src='https://i.postimg.cc/85zGRd2r/Arrow-6.png' className='arrow-img' alt='Arrow'/>
-                  <p>DOB:&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </p>
-                  <p>03/09/2004</p>
+                  <p className='details'>DOB:&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </p>
+                  <p className='details'>03/09/2004</p>
                 </div>
                 <div className='arrow-info'>
                   <img src='https://i.postimg.cc/85zGRd2r/Arrow-6.png' className='arrow-img' alt='Arrow'/>
-                  <p>Languages: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </p>
-                  <p>Xhosa ANd English</p>
+                  <p className='details'>Languages: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </p>
+                  <p className='details'>Xhosa ANd English</p>
                 </div>
                 <div className='arrow-info'>
                   <img src='https://i.postimg.cc/85zGRd2r/Arrow-6.png' className='arrow-img' alt='Arrow'/>
-                  <p>Gender: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </p>
-                  <p>Male</p>
+                  <p className='details'>Gender: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </p>
+                  <p className='details'>Male</p>
                 </div>
               </div>
             )}
@@ -61,28 +61,29 @@ const About = () => {
               <div>
                 <div className='arrow-info'>
                   <img src='https://i.postimg.cc/85zGRd2r/Arrow-6.png' className='arrow-img' alt='Arrow'/>
-                  <p>Cell No: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</p>
-                  <p>0781149972</p>
+                  <p className='details'>Cell No: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</p>
+                  <p className='details'>0781149972</p>
                 </div>
                 <div className='arrow-info'>
                   <img src='https://i.postimg.cc/85zGRd2r/Arrow-6.png' className='arrow-img' alt='Arrow'/>
-                  <p>Email: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</p>
-                  <p>ASIPHENDIMLANA40@GMAIL.COM</p>
+                  <p className='details'>Email: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</p>
+                  <p className='details'>ASIPHENDIMLANA40@GMAIL.COM</p>
                 </div>
                 <div className='arrow-info'>
                   <img src='https://i.postimg.cc/85zGRd2r/Arrow-6.png' className='arrow-img' alt='Arrow'/>
-                  <p>LinkedIn: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</p>
-                  <p>https://www.linkedin.com/in/asiphe-ndimlana-045184157/</p>
+                  <p className='details'>LinkedIn: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</p>
+                  <p className='details'>https://www.linkedin.com/in/asiphe-ndimlana-045184157/</p>
                 </div>
                 <div className='arrow-info'>
                   <img src='https://i.postimg.cc/85zGRd2r/Arrow-6.png' className='arrow-img' alt='Arrow'/>
-                  <p>Availability: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</p>
-                  <p>Any time</p>
+                  <p className='details'>Availability: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</p>
+                  <p className='details'>Any time</p>
                 </div>
               </div>
             )}
       
           <img src='https://i.postimg.cc/CL2J7BCN/Avatar-Valkyrie.webp' alt='Valkyrie' className='troop' id='Valkyrie'/>
+          <img src='https://i.postimg.cc/jjLmYt1h/Avatar-Bomber.webp' alt='Valkyrie' className='troop' id='Bomber'/>
                <img src="https://i.postimg.cc/gjzG2cgw/Earthquake-Spell-info.webp" alt="" className='troop' id='Earthquake-spell' />
           </div>
          
@@ -93,7 +94,7 @@ const About = () => {
             I have a passion for learning and am always eager to explore new technologies and stay up-to-date with industry trends.I hold a full-stack certificate from Life Choices Academy and have gained proficiency in various programming languages and frameworks, including JavaScript, Python, Vue.js Nuxt and many more.
             <br />
             <br />
-            In my professional interactions, I adopt a formal tone of voice and always strive for clean and efficient code. When I'm not busy coding, I enjoy working on my numerous projects. I take pride in my open-source contributions, which you can explore on my GitHub profile. Feel free to reach out if you want to talk about work or just geek out in general. I'm always open to collaborating on exciting new projects and sharing knowledge with fellow tech enthusiasts.
+            In my professional interactions, I adopt a formal tone of voice and always strive for clean and efficient code. When I'm not busy coding, I enjoy working on my numerous <a href="#Projects" className='link-to-projects'>projects</a>. I take pride in my open-source contributions, which you can explore on my GitHub profile. Feel free to <a href="#Contact" className='link-to-contact'>reach out</a> if you want to talk about work or just geek out in general. I'm always open to collaborating on exciting new projects and sharing knowledge with fellow tech enthusiasts.
           </p>
       
         
